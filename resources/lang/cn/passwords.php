d5044db5e79b6b327893f7809f2eb8eb8a6d6e6a
fllywaay <z.peng.com@gmail.com>
2022-02-10 17:27:38 +0800

    Create passwords.php

---
+++ resources/lang/cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少有六个字符的长度，并与认证相匹配。',
+    'reset' => '您的密码已重置！',
+    'sent' => '我们向您发送了一封电子邮件，其中包含重置密码的链接！',
+    'token' => '重置密码的令牌无效。',
+    'user' => "我们找不到此用户的 E-mail.",
+
+];
