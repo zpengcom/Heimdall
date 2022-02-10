b35ccb0ebe6401fb666228658f89f9061f7b43dc
fllywaay <z.peng.com@gmail.com>
2022-02-10 17:16:41 +0800

    chinese
    chinese

---
+++ resources/lang/cn/app.php
@@ -0,0 +1,102 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'settings.system' => '系统',
+    'settings.appearance' => '外观',
+    'settings.miscellaneous' => '其他',
+    'settings.support' => '支持',
+    'settings.donate' => '贡献',
+    
+    'settings.version' => '版本',
+    'settings.background_image' => '背景图片',
+    'settings.window_target' => '打开链接',
+    'settings.window_target.current' => '在当前选项卡中',
+    'settings.window_target.one' => '在同一个新标签中',
+    'settings.window_target.new' => '在新标签中',
+    'settings.homepage_search' => '搜索栏',
+    'settings.search_provider' => '搜索引擎',
+    'settings.language' => '语言',
+    'settings.reset' => '重置默认值',
+    'settings.remove' => '删除',
+    'settings.search' => '搜索',
+    'settings.no_items' => '没有找到内容',
+    
+    'settings.label' => '标签',
+    'settings.value' => '值',
+    'settings.edit' => '编辑',
+    'settings.view' => '查看',
+    
+    'options.none' => '未定义',
+    'options.google' => '谷歌',
+    'options.ddg' => 'DuckDuckGo',
+    'options.bing' => '必应',
+    'options.qwant' => 'Qwant',
+    'options.startpage' => '主页',
+    'options.yes' => '是',
+    'options.no' => '否',
+    
+    'buttons.save' => '保存',
+    'buttons.cancel' => '取消',
+    'buttons.add' => '添加',
+    'buttons.upload' => '选择文件',
+    
+    'dash.pin_item' => '固定到仪表板',
+    'dash.no_apps' => '没有固定的应用',
+    'dash.link1' => '在此处添加应用',
+    'dash.link2' => '将应用固定到仪表板',
+    'dash.pinned_items' => '固定应用',
+    
+    'apps.app_list' => '应用列表',
+    'apps.view_trash' => '查看垃圾箱',
+    'apps.add_application' => '添加应用',
+    'apps.application_name' => '应用名称',
+    'apps.colour' => '颜色',
+    'apps.icon' => '图标',
+    'apps.pinned' => '固定',
+    'apps.title' => '标题',
+    'apps.hex' => '颜色值',
+    'apps.username' => '用户名',
+    'apps.password' => '密码',
+    'apps.config' => '配置',
+    'apps.enable' => '活动',
+    'apps.tag_list' => '标签列表',
+    'apps.add_tag' => '添加标签',
+    'apps.tag_name' => '标签名称',
+    'apps.tags' => '标签',
+    'apps.override' => '如果与当前网址不同',
+    
+    'url' => '网址',
+    'title' => '标题',
+    'delete' => '删除',
+    'optional' => '可选',
+    'restore' => '恢复',
+    
+    'alert.success.item_created' => '应用创建成功',
+    'alert.success.item_updated' => '应用更新成功',
+    'alert.success.item_deleted' => '应用删除成功',
+    'alert.success.item_restored' => '应用恢复成功',
+    
+    'alert.success.tag_created' => '标签创建成功',
+    'alert.success.tag_updated' => '标签更新成功',
+    'alert.success.tag_deleted' => '标签删除成功',
+    'alert.success.tag_restored' => '标签恢复成功',
+    
+    'alert.success.setting_updated' => '设置更新成功',
+    'alert.error.not_exist' => '此参数不存在',
+    
+    'alert.success.user_created' => '用户创建成功',
+    'alert.success.user_updated' => '用户更新成功',
+    'alert.success.user_deleted' => '用户删除成功',
+    'alert.success.user_restored' => '用户恢复成功',
+
+
+
+];
